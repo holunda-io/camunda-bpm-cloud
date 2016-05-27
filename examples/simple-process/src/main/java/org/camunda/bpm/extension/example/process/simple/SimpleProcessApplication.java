adf8426d72f559c12c07fee086791dc21b61df1d
Martin Günther <martin.guenther@holisticon.de>
2016-05-27 20:10:17 +0200

    feat(serviceRegistry): add eureka server config to simple process

--- examples/simple-process/src/main/java/org/camunda/bpm/extension/example/process/simple/SimpleProcessApplication.java
+++ examples/simple-process/src/main/java/org/camunda/bpm/extension/example/process/simple/SimpleProcessApplication.java
@@ -7,10 +7,11 @@ import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @ProcessApplication(name = "SimpleProcessApplication")
+@EnableEurekaClient
 public class SimpleProcessApplication extends SpringBootProcessApplication {
 
   public static void main(String[] args) {
