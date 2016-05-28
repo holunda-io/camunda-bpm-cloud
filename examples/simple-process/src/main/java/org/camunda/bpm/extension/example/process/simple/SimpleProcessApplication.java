ebdb91a8a47c086e5fd5a68b5583243fed18d1ed
Martin Günther <martin.guenther@holisticon.de>
2016-05-28 11:47:08 +0200

    feat(examples/simpleProcess): restify simple process

--- examples/simple-process/src/main/java/org/camunda/bpm/extension/example/process/simple/SimpleProcessApplication.java
+++ examples/simple-process/src/main/java/org/camunda/bpm/extension/example/process/simple/SimpleProcessApplication.java
@@ -2,9 +2,11 @@ package org.camunda.bpm.extension.example.process.simple;
 
 import org.camunda.bpm.application.ProcessApplication;
 import org.camunda.bpm.spring.boot.starter.SpringBootProcessApplication;
+import org.camunda.bpm.spring.boot.starter.rest.CamundaJerseyResourceConfig;
+import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -14,4 +16,9 @@ public class SimpleProcessApplication extends SpringBootProcessApplication {
   public static void main(String[] args) {
     SpringApplication.run(SimpleProcessApplication.class, args);
   }
+
+    @Bean
+    public ResourceConfig jerseyConfig() {
+      return new CamundaJerseyResourceConfig();
+    }
 }
