0dccb19e23feb68d8fa732dec83ffa333935698d
Martin Günther <martin.guenther@holisticon.de>
2016-05-28 20:45:16 +0200

    feat(eventService): provide /eventService/process method

--- extension/camunda-bpm-cloud-event-service/src/main/java/org/camunda/bpm/extension/cloud/event/service/EventService.java
+++ extension/camunda-bpm-cloud-event-service/src/main/java/org/camunda/bpm/extension/cloud/event/service/EventService.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 @RestController
@@ -28,6 +29,25 @@ public class EventService {
     headers.setContentType(MediaType.APPLICATION_JSON);
 
     return new HttpEntity<String>(new JSONArray(events) //
+      .toString(), headers);
+  }
+
+  @RequestMapping(produces = "application/json", value = "/eventService/process", method = RequestMethod.GET)
+  public HttpEntity<String> getProcessDefinitions(){
+    Collection<ProcessDefinition> processDefinitions = new ArrayList<ProcessDefinition>() { // anonymous subclass
+      { // engines/processDefinitions should be retrieved from service registry. For the time being, static content
+        add(new ProcessDefinition("SimpleProcess", "http://localhost:8080/", "SimpleProcess"));
+        add(new ProcessDefinition("TrivialProcess", "http://localhost:8082/", "TrivialProcess"));
+      }
+    };
+    return generateProcessDefinitionCollectionJson(processDefinitions);
+  }
+
+  private HttpEntity<String> generateProcessDefinitionCollectionJson(Collection<ProcessDefinition> processDefinitions){
+    final HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+
+    return new HttpEntity<String>(new JSONArray(processDefinitions) //
       .toString(), headers);
   }
 }
--- extension/camunda-bpm-cloud-event-service/src/main/java/org/camunda/bpm/extension/cloud/event/service/ProcessDefinition.java
+++ extension/camunda-bpm-cloud-event-service/src/main/java/org/camunda/bpm/extension/cloud/event/service/ProcessDefinition.java
@@ -4,4 +4,52 @@ package org.camunda.bpm.extension.cloud.event.service;
  * Created by martingunther on 28.05.16.
  */
 public class ProcessDefinition {
+
+  private String key;
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  private String engineUrl;
+
+  public ProcessDefinition() {
+    super();
+  }
+
+  public ProcessDefinition(String key, String engineUrl, String name) {
+    this.key = key;
+    this.engineUrl = engineUrl;
+    this.name = name;
+  }
+
+  public String getKey() {
+    return key;
+  }
+
+  public void setKey(String key) {
+    this.key = key;
+  }
+
+  public String getEngineUrl() {
+    return engineUrl;
+  }
+
+  public void setEngineUrl(String engineUrl) {
+    this.engineUrl = engineUrl;
+  }
+
+  @Override
+
+  public String toString() {
+    return "ProcessDefinition{" +
+      "key='" + key + '\'' +
+      ", engineUrl='" + engineUrl + '\'' +
+      '}';
+  }
 }
