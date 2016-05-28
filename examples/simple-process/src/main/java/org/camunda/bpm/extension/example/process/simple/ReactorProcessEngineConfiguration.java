47129ca3ba37ae034eb21f1fcf5cb25840990681
Martin Günther <martin.guenther@holisticon.de>
2016-05-28 17:47:29 +0200

    fix(examples/simpleProcess): remove unnecessary comments

--- examples/simple-process/src/main/java/org/camunda/bpm/extension/example/process/simple/ReactorProcessEngineConfiguration.java
+++ examples/simple-process/src/main/java/org/camunda/bpm/extension/example/process/simple/ReactorProcessEngineConfiguration.java
@@ -1,10 +1,7 @@
 package org.camunda.bpm.extension.example.process.simple;
 
-import javax.inject.Inject;
-
 import org.camunda.bpm.engine.impl.cfg.StandaloneInMemProcessEngineConfiguration;
 import org.camunda.bpm.extension.cloud.broadcaster.BroadcasterConfiguration;
-import org.camunda.bpm.extension.cloud.broadcaster.BroadcasterInitializer;
 import org.camunda.bpm.extension.reactor.CamundaReactor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,12 +11,8 @@ import org.springframework.context.annotation.Import;
 @Import(BroadcasterConfiguration.class)
 public class ReactorProcessEngineConfiguration {
 
-//  @Inject
-//  BroadcasterInitializer initializer;
-
   @Bean
   public StandaloneInMemProcessEngineConfiguration configuration() {
-//    initializer.initialize();
     return new StandaloneInMemProcessEngineConfiguration() {
       {
         this.getProcessEnginePlugins().add(CamundaReactor.plugin());
