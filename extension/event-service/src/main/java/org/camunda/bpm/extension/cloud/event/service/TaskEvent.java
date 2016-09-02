69b76e5fffc45b9881a71ff41574cb45c4a335ce
Jan Galinski <jan.galinski@holisticon.de>
2016-09-02 16:49:32 +0200

    switch to docker-network, allow task-uuids

--- extension/event-service/src/main/java/org/camunda/bpm/extension/cloud/event/service/TaskEvent.java
+++ extension/event-service/src/main/java/org/camunda/bpm/extension/cloud/event/service/TaskEvent.java
@@ -1,57 +1,14 @@
 package org.camunda.bpm.extension.cloud.event.service;
 
+import lombok.Data;
+
+@Data
 public class TaskEvent {
 
-  private String eventType;
-  private String taskDefinitionKey;
-  private String taskId;
-  private String formKey;
-  private String engineId;
-
-  public String getEventType() {
-    return eventType;
-  }
-
-  public void setEventType(String eventType) {
-    this.eventType = eventType;
-  }
-
-  public String getEngineId() {
-    return engineId;
-  }
-
-  public void setEngineId(String engineId) {
-    this.engineId = engineId;
-  }
-
-  public String getTaskDefinitionKey() {
-    return taskDefinitionKey;
-  }
-
-  public void setTaskDefinitionKey(String taskDefinitionKey) {
-    this.taskDefinitionKey = taskDefinitionKey;
-  }
-
-  public String getTaskId() {
-    return taskId;
-  }
-
-  public void setTaskId(String taskId) {
-    this.taskId = taskId;
-  }
-
-  public String getFormKey() {
-    return formKey;
-  }
-
-  public void setFormKey(String formKey) {
-    this.formKey = formKey;
-  }
-
-  @Override
-  public String toString() {
-    return "TaskEvent [eventType=" + eventType + ", taskDefinitionKey=" + taskDefinitionKey + ", taskId=" + taskId
-        + ", formKey=" + formKey + ", engineUrl=" + engineId + "]";
-  }
+  String eventType;
+  String taskDefinitionKey;
+  String taskId;
+  String formKey;
+  String engineId;
 
 }
