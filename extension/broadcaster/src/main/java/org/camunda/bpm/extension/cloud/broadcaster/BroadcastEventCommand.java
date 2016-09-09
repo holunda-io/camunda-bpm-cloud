050e457f5eb46d09fe3bff3e06b50e2b97d8a3e2
Jan Galinski <jan.galinski@holisticon.de>
2016-09-09 17:02:27 +0200

    refactor service names

--- extension/broadcaster/src/main/java/org/camunda/bpm/extension/cloud/broadcaster/BroadcastEventCommand.java
+++ extension/broadcaster/src/main/java/org/camunda/bpm/extension/cloud/broadcaster/BroadcastEventCommand.java
@@ -1,6 +1,6 @@
 package org.camunda.bpm.extension.cloud.broadcaster;
 
-import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.web.client.RestTemplate;
 
