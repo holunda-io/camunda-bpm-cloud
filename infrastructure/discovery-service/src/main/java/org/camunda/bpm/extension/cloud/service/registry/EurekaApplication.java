efc6142680c86cfaaed57936d384b7ad55238f15
Jan Galinski <jan.galinski@holisticon.de>
2017-04-20 15:32:15 +0200

    move edge, config, discovery to infrastructure, fixes #31

--- infrastructure/discovery-service/src/main/java/org/camunda/bpm/extension/cloud/service/registry/EurekaApplication.java
+++ infrastructure/discovery-service/src/main/java/org/camunda/bpm/extension/cloud/service/registry/EurekaApplication.java
@@ -2,8 +2,6 @@ package org.camunda.bpm.extension.cloud.service.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
