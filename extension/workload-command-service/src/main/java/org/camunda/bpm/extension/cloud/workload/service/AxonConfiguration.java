30c6a0078e3db00e1f0c025d045184301a8a078a
Jan Galinski <jan.galinski@holisticon.de>
2017-05-08 00:16:31 +0200

    map aggregate to listener

--- extension/workload-command-service/src/main/java/org/camunda/bpm/extension/cloud/workload/service/AxonConfiguration.java
+++ extension/workload-command-service/src/main/java/org/camunda/bpm/extension/cloud/workload/service/AxonConfiguration.java
@@ -1,11 +1,31 @@
 package org.camunda.bpm.extension.cloud.workload.service;
 
+import com.rabbitmq.client.Channel;
+import lombok.extern.slf4j.Slf4j;
+import org.axonframework.amqp.eventhandling.AMQPMessageConverter;
+import org.axonframework.amqp.eventhandling.spring.SpringAMQPMessageSource;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableAutoConfiguration
+@Slf4j
 public class AxonConfiguration {
 
+  @Bean
+  public SpringAMQPMessageSource myQueueMessageSource(AMQPMessageConverter messageConverter) {
+    return new SpringAMQPMessageSource(messageConverter) {
+
+      @RabbitListener(queues = "${camunda.bpm.cloud.amqp.queue}")
+      @Override
+      public void onMessage(Message message, Channel channel) throws Exception {
+
+        log.info("receiving: {}#{}", message, channel);
+        super.onMessage(message, channel);
+      }
+    };
+  }
 
 }
